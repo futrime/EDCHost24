56f858b2c5a6a08b61481d5ce601609a516189ac
Alfred Dai <drf21@mails.tsinghua.edu.cn>
2022-09-20 16:40:11 +0800

    Replace files

--- Source/Car.cs
+++ Source/Car.cs
@@ -3,186 +3,354 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
-namespace EDCHOST22
+namespace EDCHOST24
 {
-    // 队名
+    //队名
     public enum Camp
     {
         NONE = 0, A, B
     };
-    public class Car // 选手的车
+
+
+    // STL: Store a series of position of car
+    public class MyQueue <T>
     {
-        public const int CARIN_CREDIT = 10;         // 车进入中心矿区可以得到10分；
-        public const int LOAD1_CREDIT = 20;        // 第一回合，走到一个金矿处可以得到20分;
-        public const int LOAD2_CREDIT = 5;       // 第二回合，收集到一个金矿可以得到5分；
-        public const int UNLOAD_CREDIT = 10;     // 第一/二回合，将金矿运送到指定地点可以得到10分/金矿;
-        public const int AHEAD_CREDIT_PS = 1;         // 提前完成第一回合任务，每提前1sec加1分；
-        public const int BEACON_CREDIT = 5;      // 放置信标可以得到5分；
-        public const int BEACON_PENALTY = 50;     // 触碰到信标惩罚50分；
-        public const int FOUL_PENALTY = 50;      // 犯规扣分50分；
-        public const int BEACON_LIMIT = 3;      // 最多放置3个信标；
-        public const int MINE_LIMIT = 10;        // 最多载10个金矿；
+        private int MAX_LONGTH;
 
-       
+        private List<T> mItem;
 
-        public Dot mPos;                // 当前位置
-        public Dot mLastPos;            // 上一帧的位置，用于判断碰撞
-        public Dot mTransPos;           // 发送的位置
-        // public Dot mLastOneSecondPos;      // 上一秒的位置，便于实现1Hz发送
-        // public Dot mTransPos;               // 最终发送的位置，10Hz则为mPos，1Hz则为mLastOneSecondPos
-        public Camp MyCamp;               // A or B get、set直接两个封装好的函数
-        public int MyScore;               // 得分
-        public int mMine1Load;             // 小车在第一回合成功收集金矿个数
-        public int mMine1Unload;           // 小车在第一回合成功运送金矿的个数
-        public int[] mMine2Load = new int[Mine.MINE_TYPE];    // 小车在第二回合成功收集金矿的个数（分四类）
-        public int[] mMine2Unload = new int[Mine.MINE_TYPE];            // 小车在第二回合成功运送金矿的个数（分四类）
-        public int mAheadSec;              // 第一回合提前完成的秒数
-        public int mTaskState;            // 小车任务，0为第一回合，1为第二回合
-        public int[] mMineState = new int[Mine.MINE_TYPE];       // 小车上载有金矿的个数（分四类）
-        public int mIsInMaze;             // 小车所在的区域 0在迷宫外 1在迷宫内
-        public int mIsInField;            // 小车目前在不在场地内 0不在场地内 1在场地内
-        public int mCrossBeaconCount;      // 小车触碰信标的次数
-        public int mFoulCount;            // 犯规按键次数
-        //public int mRightPos;             // 小车现在的位置信息是否是正确的，0为不正确的，1为正确的
-        //public int mRightPosCount;        // 用于记录小车位置是否该正确了（实现1Hz）
-        public int WhetherCarIn;          // 记录小车是否进入了迷宫
-        public int mBeaconCount;          // 记录小车放置信标数目
-        public int mRightPos;           // 车的坐标是否正确
-
-        public int mOverTime;          // 第一回合超过1min的时间
-
-
-        public Car(Camp c, int task)
+        public MyQueue(int _MaxLongth)
         {
-
-            MyCamp = c;
-            mPos = new Dot(0, 0);
-            mLastPos = new Dot(0, 0);
-            mTransPos = new Dot(0, 0);
-            MyScore = 0;
-            mMine1Load = 0;
-            mMine1Unload = 0;
-            for (int i = 0; i < mMine2Load.Length; i++)
+            if (_MaxLongth < 1)
             {
-                mMine2Load[i] = 0;
-                mMine2Unload[i] = 0;
-                mMineState[i] = 0;
+                throw new Exception("MaxLongth is expected to be larger than 0");
             }
-            mAheadSec = 0;
-            mTaskState = task;
-            mIsInField = 0;
-            mIsInMaze = 0;
-            mCrossBeaconCount = 0;
-            mFoulCount = 0;
-            WhetherCarIn = 0;
-            mBeaconCount = 0;
-            mRightPos = 0;
-
-            mOverTime = 0;
-        }
-        public void UpdateLastPos() // 更新上一帧位置
-        {
-            mLastPos = mPos;
-        }
-        
-        public void SetPos(Dot pos) // 设置当前位置
-        {
-            mPos = pos;
+            MAX_LONGTH = _MaxLongth;
+            mItem = new List<T>(); 
         }
 
-        public void AddCrossBeacon() // 触碰信标次数
+        public void Enqueue (T _item)
         {
-            mCrossBeaconCount++;
-            UpdateScore();
-        }
-        public void AddMineLoad(int round, MineType type = MineType.A)  // 根据回合数(round=0代表回合1，round=1代表回合2，type=0/1/2/3表示种类），增加收集矿物次数1次
-        {
-            if (round == 0)
+            if (mItem.Count < MAX_LONGTH)
             {
-                mMine1Load ++;
+                mItem.Add(_item);
+            }
+            else if (mItem.Count == MAX_LONGTH)
+            {
+                mItem.RemoveAt(0);
+                mItem.Add(_item);
+            }
+        }
+
+        public T Item (int _index)
+        {
+            if (_index < mItem.Count && _index >= 0)
+            {
+                return mItem[_index];
+            }
+            else if (_index <= -1 && _index >= -mItem.Count)
+            {
+                return mItem[mItem.Count + _index];
             }
             else
             {
-                mMine2Load[(int)type] ++;
+                throw new Exception("PosQueue: index is out of range");
             }
-            UpdateScore();
         }
 
-        public void AddMineUnload(int round, MineType type=MineType.A)   // 根据回合数(round=0代表回合1，round=1代表回合2，type=0/1/2/3表示种类）
+        public int Count()
         {
-            if (round == 0)
-            {
-                mMine1Unload += mMine1Unload < 2 ? 2 : 0;
-            }
-            else
-            {
-                mMine2Unload[(int)type] += mMineState[(int)type];
-            }
-            UpdateScore();
+            return mItem.Count;
         }
-        public void AddFoulCount()  // 增加一次犯规
+
+        public void Clear()
         {
-            mFoulCount++;
-            UpdateScore();
-        }  
+            mItem.Clear();
+        }
+    }
+
+
+
+
+
+    public class Car //选手的车
+    {
+
+        // the object of package and picked by car and first collision time
+        private class PackagesAndTime
+        {
+            public Package mPkg;
+            public int mFirstCollisionTime;
+
+            public PackagesAndTime (Package _pkg, int _FirstCollisionTime = -1)
+            {
+                mPkg = _pkg;
+                mFirstCollisionTime = _FirstCollisionTime;
+            }
+        }
+
+        public const int RUN_CREDIT = 10;          //小车启动可以得到10分;
+        public const int PICK_CREDIT = 5;          //接到一笔订单得5分;
+        public const int CHARGE_CREDIT = 5;        // credit for set a charge station
+        public const int ON_BLACk_LINE_PENALTY = 10;    
+        public const int IN_OPPONENT_STATION_PENALTY = 10; // in cm per frame
+        public const int IN_OBSTACLE_PENALTY = 10; // in cm per frame
+        public const int MARK_PENALTY = 50;        
+        public const int MAX_PKG_COUNT = 5;        
+        public const int ENERGY_EXHAUSTION_PENALTY = 50; // 50 ms per cm
         
-        public void AddBeaconCount()         // 放置了1个信标
-        {
-            mBeaconCount += mBeaconCount >= BEACON_LIMIT ? 0 : 1;
-            UpdateScore();
-        }
-        public void SetAheadSec(int _sec)   // 设置提前完成第一回合任务的秒数
-        {
-            mAheadSec = _sec;
-            UpdateScore();
-        }
-        public int mMineStateSum()  // 载有金矿总数
-        {
-            return mMineState.Sum();
-        }
-        public bool IsMineStateFull()        // 是否满载
-        {
-            return mMineStateSum() >= MINE_LIMIT;
-        }
-        public void ClearMineTypeState(MineType type)  // 清空特定种类的矿（type=0/1/2/3表示种类）
-        {
-            mMineState[(int)type] = 0;
-        }
-        public void ClearMineState()
-        {
-            for (int i = 0; i < Mine.MINE_TYPE; i++)    // 清空所有矿的种类
-            {
-                mMineState[i] = 0;
-            }
-        }
-        public void AddMineState(MineType type = MineType.A)  // 增加当前载矿数（type=0/1/2/3表示种类）（第一回合默认使用A种类金矿）
-        {
-            mMineState[(int)type] += IsMineStateFull() ? 0 : 1;
-        }
-        public void SetCarIn()          // 车进入中心矿区
-        {
-            WhetherCarIn = 1;
-            UpdateScore();
-        }
+
+
+        public const int COLLISION_RADIUS = 8;
+        public const int COLLISION_DETECTION_TIME = 1000; // in ms
+        public const int MAX_MILEAGE = 2000; // in cm
+
+
+        private MyQueue<Dot> mQueuePos;   // series of location
+        public Camp mCamp;               //A or B get、set直接两个封装好的函数
+        private int mScore;               //得分
+        private int mMileage;              //小车续航里程
+        private List<PackagesAndTime> mPickedPackages; // package picked by car
+
+        //Flag of whether the car is able to run
+        private bool mIsAbleToRun;
+
+
+        // Flags of Location
+        // Locations where car would get penalty
+        private bool mIsOnBlackLine;   
+        private bool mIsInOpponentChargeStation;
+        private bool mIsInObstacle;       
         
-        public void setOverTime(int _sec)
+        
+        public MyQueue<bool> mFlagIsInChargeStation;
+
+
+        private int mGameTime;
+
+
+        /********************************************
+        Interface
+        *********************************************/
+        public Car(Camp c)
         {
-            mOverTime = _sec;
-            UpdateScore();
-        }
-        public void UpdateScore()
-        {
-            // 初赛修正
+            mQueuePos = new MyQueue<Dot>(10);
+            mCamp = c;
+            mScore = 0;
+            mMileage = MAX_MILEAGE;
+            mPickedPackages = new List<PackagesAndTime>();
             
-            MyScore = mMine1Load * LOAD1_CREDIT + mMine2Load.Sum() * LOAD2_CREDIT         // 收集到矿的分数
-                + (mMine1Unload + mMine2Unload.Sum()) * UNLOAD_CREDIT    // 运送成功的分数
-                - mCrossBeaconCount * BEACON_PENALTY                    // 触碰信标惩罚的分数
-                - mFoulCount * FOUL_PENALTY                             // 犯规扣分
-                + WhetherCarIn * CARIN_CREDIT                           // 车进入中心矿区的分数
-                + mAheadSec * AHEAD_CREDIT_PS                          // 提前完成第一回合任务的分数
-                + mBeaconCount * BEACON_CREDIT                         // 放置信标的分数
-                -(mOverTime / 5) * AHEAD_CREDIT_PS;                     // 第一回合超时的扣分
+            // Flags
+            mIsAbleToRun = false;
+            mIsOnBlackLine = false;
+            mIsInOpponentChargeStation = false;
+            mIsInObstacle = false;
+            mFlagIsInChargeStation = new MyQueue<bool>(10);
+
+            mGameTime = -1;
+        }
+
+        public void Reset()
+        {
+            mQueuePos.Clear();
+
+            mScore = 0;
+            mMileage = MAX_MILEAGE;
+            mPickedPackages.Clear();
+
+            // Flags
+            mIsAbleToRun = false;
+            mIsOnBlackLine = false;
+            mIsInOpponentChargeStation = false;
+            mIsInObstacle = false;
+            mFlagIsInChargeStation.Clear();
+
+            mGameTime = -1;
+        }
+
+        public void Update(Dot _CarPos, int _GameTime, bool _IsOnBlackLine, 
+            bool _IsInObstacle, bool _IsInOpponentStation, bool _IsInChargeStation, 
+            ref List<Package> _PackagesRemain, out int _TimePenalty)
+        {
+            mGameTime = _GameTime;
+
+            UpdatePos(_CarPos);
+
+            if (!mIsAbleToRun) 
+            {
+                AbleToRun();
+            }
+
+            _TimePenalty = 0;
+
+            //action
+            PickPackage(_CarPos, ref _PackagesRemain);
+            DropPackage(_CarPos);
+            UpdateMileage(out _TimePenalty);
+            Charge(_IsInChargeStation);
+
+            // Penalty
+            OnBlackLinePenaly(_IsOnBlackLine);
+            InOpponentStationPenalty(_IsInOpponentStation);
+            InObstaclePenalty(_IsInObstacle);
+        }
+
+        public int GetScore ()
+        {
+            return mScore;
+        }
+
+        public void GetMark()
+        {
+            mScore -= MARK_PENALTY;
+        }
+
+        public void SetChargeStation ()
+        {
+            mScore += CHARGE_CREDIT;
+        }
+
+        public Dot CurrentPos()
+        {
+            return mQueuePos.Item(-1);
+        }
+
+        public Package GetPackageOnCar(int _index)
+        {
+            if (_index >= mPickedPackages.Count)
+            {
+                return new Package();
+            }
+            else
+            {
+                return mPickedPackages[_index].mPkg;
+            }
+        }
+
+        public int GetPackageCount()
+        {
+            return mPickedPackages.Count;
+        }
+
+        public int GetMileage()
+        {
+            return mMileage;
+        }
+
+        /********************************************
+        Private Functions
+        *********************************************/
+
+        private void UpdatePos (Dot _CarPos)
+        {
+            mQueuePos.Enqueue(_CarPos);
+        }
+
+        private void AbleToRun()
+        {
+            if (!mIsAbleToRun && mQueuePos.Count() > 1 && 
+            Dot.Distance(mQueuePos.Item(-1), mQueuePos.Item(-2)) > 0)
+            {
+                mScore += RUN_CREDIT;
+                mIsAbleToRun = true;
+            }
+        }
+
+        private void PickPackage(Dot _CarPos, ref List<Package> _PackagesRemain)      //拾取外卖
+        {
+            foreach (var pkg in _PackagesRemain)
+            {
+                if (pkg.Distance2Departure(_CarPos) <= COLLISION_RADIUS && 
+                    mPickedPackages.Count <= MAX_PKG_COUNT)
+                {
+                    mPickedPackages.Add(new PackagesAndTime (pkg));
+                    _PackagesRemain.Remove(pkg);
+                    mScore += PICK_CREDIT;
+                }
+            }
+        }
+
+        private void DropPackage(Dot _CarPos)      //送达外卖 
+        {
+            foreach (var PkgAndTime in mPickedPackages)
+            {
+                if (PkgAndTime.mPkg.Distance2Destination(_CarPos) <= COLLISION_RADIUS)
+                {
+                    if (PkgAndTime.mFirstCollisionTime != -1 && 
+                        mGameTime - PkgAndTime.mFirstCollisionTime > COLLISION_DETECTION_TIME)
+                    {
+                        mPickedPackages.Remove(PkgAndTime);
+                        mScore += PkgAndTime.mPkg.GetPackageScore(mGameTime);
+                    }
+                    else if (PkgAndTime.mFirstCollisionTime == -1)
+                    {
+                        PkgAndTime.mFirstCollisionTime = mGameTime;
+                    }
+                }
+                else if (PkgAndTime.mFirstCollisionTime != -1)
+                {
+                    PkgAndTime.mFirstCollisionTime = -1;
+                }
+            }
+        }
+
+        private void UpdateMileage (out int _Time_Penalty)
+        {
+            int DeltaDistance = Dot.Distance(mQueuePos.Item(-1), mQueuePos.Item(-2));
+            mMileage -= DeltaDistance;
+            if (mMileage < 0)
+            {
+                _Time_Penalty = DeltaDistance * ENERGY_EXHAUSTION_PENALTY;
+            }
+            else
+            {
+                _Time_Penalty = 0;
+            }
+        }
+
+        private void Charge (bool IsInChargeStation)
+        {
+            mFlagIsInChargeStation.Enqueue(IsInChargeStation);
+            for (int i = 0; i < mFlagIsInChargeStation.Count();i++)
+            {
+                if (!mFlagIsInChargeStation.Item(i))
+                {
+                    return;
+                }
+            }
+
+            mMileage = MAX_MILEAGE;
+        }
+
+        private void OnBlackLinePenaly (bool IsOnBlackLine)
+        {
+            if (IsOnBlackLine && !mIsOnBlackLine)
+            {
+                mIsOnBlackLine = IsOnBlackLine;
+            } 
+            else if (!IsOnBlackLine && mIsOnBlackLine)
+            {
+                mIsOnBlackLine = IsOnBlackLine;
+                mScore -= ON_BLACk_LINE_PENALTY;
+            }
+        }
+
+        private void InOpponentStationPenalty (bool IsInOpponentStation)
+        {
+            if (IsInOpponentStation)
+            {
+                mMileage -= IN_OPPONENT_STATION_PENALTY;
+            }
+        }
+
+        private void InObstaclePenalty (bool IsInObstacle)
+        {
+            if (mIsInObstacle)
+            {
+                mMileage -= IN_OBSTACLE_PENALTY;
+            }
         }
     }
 }
---
+++ Source/Package.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using EDCHOST24.Dot; 不需要
+
+namespace EDCHOST24
+{
+    //Package
+    public class Package
+    {
+        public const int ARRIVE_EASY_CREDIT = 20;   // scores for different levels of packages
+        public const int ARRIVE_NORMAL_CREDIT = 25;
+        public const int ARRIVE_HARD_CREDIT = 30;
+
+        public const int OVER_TIME_PENALTY = 5; // per second
+
+        private Dot mDeparture;     // Departure of Package
+        private Dot mDestination;   // Destination of Package
+
+        // all times are in ms
+        private int mGenerationTime;
+        private int mScheduledDeliveryTime;
+
+        private int mPackageLevel;  //judge the package is easy/normal/hard to be arrived //0-easy; 1-normal; 2-hard;
+
+        private int mScheduledScore;
+        private int mIndentityCode;
+
+        // only called when need to generate default package
+        public Package()
+        {
+            mDeparture = new Dot(0xff, 0xff);
+            mDestination = new Dot (0xff, 0xff);
+            mGenerationTime = 0xff;
+            mScheduledDeliveryTime = 0xff;
+            mPackageLevel = 0;
+            mScheduledScore = 0;
+        }
+
+        public Package(Dot inDeparturePos, Dot inDestinationPos, int inGenerationTime, int inIndentityCode)
+        {
+            mDeparture = inDeparturePos;
+            mDestination = inDestinationPos;
+            mGenerationTime = inGenerationTime;
+            mScheduledDeliveryTime = 20 * Dot.Distance(mDeparture, mDestination) + 1000;
+            mIndentityCode = inIndentityCode;
+
+            //judge level
+            if (mDeparture.x >= 40 && mDeparture.x <= 214 && mDeparture.y >= 40 && mDeparture.y <= 214
+                && mDestination.x >= 40 && mDestination.x <= 214 && mDestination.y >= 40 && mDestination.y <= 214)
+            {
+                if (Dot.Distance(mDeparture, mDestination) <= 120)
+                {
+                    mPackageLevel = 0;
+                }
+                else
+                {
+                    mPackageLevel = 1;
+                }
+            }
+            else
+            {
+                mPackageLevel = 2;
+            }
+            //judge score
+            switch (mPackageLevel)
+            {
+                case 0: mScheduledScore = ARRIVE_EASY_CREDIT; break;
+                case 1: mScheduledScore = ARRIVE_NORMAL_CREDIT; break;
+                case 2: mScheduledScore = ARRIVE_HARD_CREDIT; break;
+            }
+        }
+
+        public Dot Departure()
+        {
+            return mDeparture;
+        }
+
+        public Dot Destination()
+        {
+            return mDestination;
+        }
+
+        public int GenerationTime()
+        {
+            return mGenerationTime;
+        }
+
+        public int ScheduledDeliveryTime()
+        {
+            return mScheduledDeliveryTime;
+        }
+
+        public int IndentityCode()
+        {
+            return mIndentityCode;
+        }
+
+        public int Distance2Departure(Dot _CarPos)
+        {
+            return Dot.Distance(_CarPos, mDeparture);
+        }
+
+        public int Distance2Destination(Dot _CarPos)
+        {
+            return Dot.Distance(_CarPos, mDestination);
+        }
+
+        public int GetPackageScore(int _ArrivalTime)
+        {
+            int PackageScore = 0;
+
+            if (_ArrivalTime <= mScheduledDeliveryTime)
+            {
+                PackageScore = mScheduledScore;
+            }
+            else
+            {
+                PackageScore = mScheduledScore + (mScheduledDeliveryTime - _ArrivalTime) * OVER_TIME_PENALTY / 1000;
+            }
+
+            return PackageScore;
+        }
+    }
+}
