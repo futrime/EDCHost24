be5085c98eb0264c437eefa72b7edcaee800c2ef
Futrime <futrime@outlook.com>
2022-09-23 13:50:34 +0800

    Fix some warnings

--- Source/Dot.cs
+++ Source/Dot.cs
@@ -7,7 +7,6 @@ public class Dot
     public int x;
     public int y;
 
-
     public Dot(int _x = 0, int _y = 0)
     {
         x = _x;
@@ -28,5 +27,15 @@ public class Dot
     {
         return (int)Math.Sqrt((A.x - B.x) * (A.x - B.x)
             + (A.y - B.y) * (A.y - B.y));
+    }
+
+    public override bool Equals(object obj)
+    {
+        return base.Equals(obj);
+    }
+
+    public override int GetHashCode()
+    {
+        return base.GetHashCode();
     }
 }
