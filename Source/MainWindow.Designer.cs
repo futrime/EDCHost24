4c9e0a39b0831394af14828ff802ce8557f424eb
Futrime <futrime@outlook.com>
2022-09-30 08:29:42 +0800

    Add icon

--- Source/MainWindow.Designer.cs
+++ Source/MainWindow.Designer.cs
@@ -15,6 +15,7 @@ partial class MainWindow
     private void InitializeComponent()
     {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
             this.pictureBoxMonitor = new System.Windows.Forms.PictureBox();
             this.timer = new System.Windows.Forms.Timer(this.components);
             this.buttonStart = new System.Windows.Forms.Button();
@@ -51,7 +52,7 @@ partial class MainWindow
             this.pictureBoxMonitor.Location = new System.Drawing.Point(0, 0);
             this.pictureBoxMonitor.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBoxMonitor.Name = "pictureBoxMonitor";
-            this.pictureBoxMonitor.Size = new System.Drawing.Size(915, 662);
+            this.pictureBoxMonitor.Size = new System.Drawing.Size(1144, 828);
             this.pictureBoxMonitor.TabIndex = 0;
             this.pictureBoxMonitor.TabStop = false;
             this.pictureBoxMonitor.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBoxMonitor_MouseClick);
@@ -64,10 +65,10 @@ partial class MainWindow
             // buttonStart
             // 
             this.buttonStart.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.buttonStart.Location = new System.Drawing.Point(165, 10);
+            this.buttonStart.Location = new System.Drawing.Point(206, 12);
             this.buttonStart.Margin = new System.Windows.Forms.Padding(2);
             this.buttonStart.Name = "buttonStart";
-            this.buttonStart.Size = new System.Drawing.Size(160, 40);
+            this.buttonStart.Size = new System.Drawing.Size(200, 50);
             this.buttonStart.TabIndex = 1;
             this.buttonStart.Text = "Start";
             this.buttonStart.UseVisualStyleBackColor = true;
@@ -77,10 +78,10 @@ partial class MainWindow
             // 
             this.buttonPause.Cursor = System.Windows.Forms.Cursors.Hand;
             this.buttonPause.Enabled = false;
-            this.buttonPause.Location = new System.Drawing.Point(165, 53);
+            this.buttonPause.Location = new System.Drawing.Point(206, 66);
             this.buttonPause.Margin = new System.Windows.Forms.Padding(2);
             this.buttonPause.Name = "buttonPause";
-            this.buttonPause.Size = new System.Drawing.Size(160, 40);
+            this.buttonPause.Size = new System.Drawing.Size(200, 50);
             this.buttonPause.TabIndex = 2;
             this.buttonPause.Text = "Pause";
             this.buttonPause.UseVisualStyleBackColor = true;
@@ -90,10 +91,10 @@ partial class MainWindow
             // 
             this.buttonContinue.Cursor = System.Windows.Forms.Cursors.Hand;
             this.buttonContinue.Enabled = false;
-            this.buttonContinue.Location = new System.Drawing.Point(165, 96);
+            this.buttonContinue.Location = new System.Drawing.Point(206, 120);
             this.buttonContinue.Margin = new System.Windows.Forms.Padding(2);
             this.buttonContinue.Name = "buttonContinue";
-            this.buttonContinue.Size = new System.Drawing.Size(160, 40);
+            this.buttonContinue.Size = new System.Drawing.Size(200, 50);
             this.buttonContinue.TabIndex = 3;
             this.buttonContinue.Text = "Continue";
             this.buttonContinue.UseVisualStyleBackColor = true;
@@ -108,7 +109,7 @@ partial class MainWindow
             this.labelScoreVehicleA.Location = new System.Drawing.Point(0, 0);
             this.labelScoreVehicleA.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.labelScoreVehicleA.Name = "labelScoreVehicleA";
-            this.labelScoreVehicleA.Size = new System.Drawing.Size(428, 65);
+            this.labelScoreVehicleA.Size = new System.Drawing.Size(535, 81);
             this.labelScoreVehicleA.TabIndex = 8;
             this.labelScoreVehicleA.Text = "labelScoreVehicleA";
             // 
@@ -118,20 +119,20 @@ partial class MainWindow
             this.labelScoreVehicleB.BackColor = System.Drawing.Color.Blue;
             this.labelScoreVehicleB.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.labelScoreVehicleB.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.labelScoreVehicleB.Location = new System.Drawing.Point(0, 65);
+            this.labelScoreVehicleB.Location = new System.Drawing.Point(0, 81);
             this.labelScoreVehicleB.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.labelScoreVehicleB.Name = "labelScoreVehicleB";
-            this.labelScoreVehicleB.Size = new System.Drawing.Size(425, 65);
+            this.labelScoreVehicleB.Size = new System.Drawing.Size(530, 81);
             this.labelScoreVehicleB.TabIndex = 10;
             this.labelScoreVehicleB.Text = "labelScoreVehicleB";
             // 
             // buttonCalibration
             // 
             this.buttonCalibration.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.buttonCalibration.Location = new System.Drawing.Point(0, 96);
+            this.buttonCalibration.Location = new System.Drawing.Point(0, 120);
             this.buttonCalibration.Margin = new System.Windows.Forms.Padding(2);
             this.buttonCalibration.Name = "buttonCalibration";
-            this.buttonCalibration.Size = new System.Drawing.Size(160, 40);
+            this.buttonCalibration.Size = new System.Drawing.Size(200, 50);
             this.buttonCalibration.TabIndex = 11;
             this.buttonCalibration.Text = "Calibrate";
             this.buttonCalibration.UseVisualStyleBackColor = true;
@@ -140,10 +141,10 @@ partial class MainWindow
             // buttonFoul
             // 
             this.buttonFoul.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.buttonFoul.Location = new System.Drawing.Point(0, 53);
+            this.buttonFoul.Location = new System.Drawing.Point(0, 66);
             this.buttonFoul.Margin = new System.Windows.Forms.Padding(2);
             this.buttonFoul.Name = "buttonFoul";
-            this.buttonFoul.Size = new System.Drawing.Size(160, 40);
+            this.buttonFoul.Size = new System.Drawing.Size(200, 50);
             this.buttonFoul.TabIndex = 13;
             this.buttonFoul.Text = "Foul";
             this.buttonFoul.UseVisualStyleBackColor = true;
@@ -153,10 +154,10 @@ partial class MainWindow
             // 
             this.buttonEnd.Cursor = System.Windows.Forms.Cursors.Hand;
             this.buttonEnd.Enabled = false;
-            this.buttonEnd.Location = new System.Drawing.Point(165, 141);
+            this.buttonEnd.Location = new System.Drawing.Point(206, 176);
             this.buttonEnd.Margin = new System.Windows.Forms.Padding(2);
             this.buttonEnd.Name = "buttonEnd";
-            this.buttonEnd.Size = new System.Drawing.Size(160, 40);
+            this.buttonEnd.Size = new System.Drawing.Size(200, 50);
             this.buttonEnd.TabIndex = 14;
             this.buttonEnd.Text = "End";
             this.buttonEnd.UseVisualStyleBackColor = true;
@@ -165,10 +166,10 @@ partial class MainWindow
             // buttonReset
             // 
             this.buttonReset.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.buttonReset.Location = new System.Drawing.Point(0, 10);
+            this.buttonReset.Location = new System.Drawing.Point(0, 12);
             this.buttonReset.Margin = new System.Windows.Forms.Padding(2);
             this.buttonReset.Name = "buttonReset";
-            this.buttonReset.Size = new System.Drawing.Size(160, 40);
+            this.buttonReset.Size = new System.Drawing.Size(200, 50);
             this.buttonReset.TabIndex = 16;
             this.buttonReset.Text = "Reset";
             this.buttonReset.UseVisualStyleBackColor = true;
@@ -177,9 +178,10 @@ partial class MainWindow
             // buttonSettings
             // 
             this.buttonSettings.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.buttonSettings.Location = new System.Drawing.Point(0, 141);
+            this.buttonSettings.Location = new System.Drawing.Point(0, 176);
+            this.buttonSettings.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.buttonSettings.Name = "buttonSettings";
-            this.buttonSettings.Size = new System.Drawing.Size(160, 40);
+            this.buttonSettings.Size = new System.Drawing.Size(200, 50);
             this.buttonSettings.TabIndex = 17;
             this.buttonSettings.Text = "Settings";
             this.buttonSettings.UseVisualStyleBackColor = true;
@@ -190,10 +192,10 @@ partial class MainWindow
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.Blue;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(0, 65);
+            this.label1.Location = new System.Drawing.Point(0, 81);
             this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(327, 65);
+            this.label1.Size = new System.Drawing.Size(403, 81);
             this.label1.TabIndex = 6;
             this.label1.Text = "                       ";
             // 
@@ -205,7 +207,7 @@ partial class MainWindow
             this.label2.Location = new System.Drawing.Point(0, 0);
             this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(327, 65);
+            this.label2.Size = new System.Drawing.Size(403, 81);
             this.label2.TabIndex = 5;
             this.label2.Text = "                       ";
             // 
@@ -213,9 +215,10 @@ partial class MainWindow
             // 
             this.labelGameTime.AutoSize = true;
             this.labelGameTime.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.labelGameTime.Location = new System.Drawing.Point(0, 171);
+            this.labelGameTime.Location = new System.Drawing.Point(0, 214);
+            this.labelGameTime.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelGameTime.Name = "labelGameTime";
-            this.labelGameTime.Size = new System.Drawing.Size(355, 65);
+            this.labelGameTime.Size = new System.Drawing.Size(446, 81);
             this.labelGameTime.TabIndex = 18;
             this.labelGameTime.Text = "labelGameTime";
             // 
@@ -233,9 +236,10 @@ partial class MainWindow
             this.panel1.Controls.Add(this.labelGameTime);
             this.panel1.Controls.Add(this.labelGameHalf);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
-            this.panel1.Location = new System.Drawing.Point(10, 10);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(331, 662);
+            this.panel1.Size = new System.Drawing.Size(414, 828);
             this.panel1.TabIndex = 20;
             // 
             // label5
@@ -243,9 +247,10 @@ partial class MainWindow
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.label5.ForeColor = System.Drawing.SystemColors.GrayText;
-            this.label5.Location = new System.Drawing.Point(0, 333);
+            this.label5.Location = new System.Drawing.Point(0, 416);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(203, 32);
+            this.label5.Size = new System.Drawing.Size(255, 41);
             this.label5.TabIndex = 23;
             this.label5.Text = "Remaining Power:";
             // 
@@ -254,9 +259,10 @@ partial class MainWindow
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.label4.ForeColor = System.Drawing.SystemColors.GrayText;
-            this.label4.Location = new System.Drawing.Point(0, 236);
+            this.label4.Location = new System.Drawing.Point(0, 295);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(131, 32);
+            this.label4.Size = new System.Drawing.Size(163, 41);
             this.label4.TabIndex = 22;
             this.label4.Text = "Game Half:";
             // 
@@ -265,17 +271,19 @@ partial class MainWindow
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.label3.ForeColor = System.Drawing.SystemColors.GrayText;
-            this.label3.Location = new System.Drawing.Point(0, 139);
+            this.label3.Location = new System.Drawing.Point(0, 174);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(192, 32);
+            this.label3.Size = new System.Drawing.Size(238, 41);
             this.label3.TabIndex = 21;
             this.label3.Text = "Remaining Time:";
             // 
             // progressBarRemainingPowerRatio
             // 
-            this.progressBarRemainingPowerRatio.Location = new System.Drawing.Point(0, 368);
+            this.progressBarRemainingPowerRatio.Location = new System.Drawing.Point(0, 460);
+            this.progressBarRemainingPowerRatio.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.progressBarRemainingPowerRatio.Name = "progressBarRemainingPowerRatio";
-            this.progressBarRemainingPowerRatio.Size = new System.Drawing.Size(325, 23);
+            this.progressBarRemainingPowerRatio.Size = new System.Drawing.Size(406, 29);
             this.progressBarRemainingPowerRatio.TabIndex = 20;
             // 
             // panel3
@@ -289,19 +297,20 @@ partial class MainWindow
             this.panel3.Controls.Add(this.buttonCalibration);
             this.panel3.Controls.Add(this.buttonFoul);
             this.panel3.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panel3.Location = new System.Drawing.Point(0, 481);
+            this.panel3.Location = new System.Drawing.Point(0, 602);
+            this.panel3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(331, 181);
+            this.panel3.Size = new System.Drawing.Size(414, 226);
             this.panel3.TabIndex = 1;
             // 
             // labelGameHalf
             // 
             this.labelGameHalf.AutoSize = true;
             this.labelGameHalf.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.labelGameHalf.Location = new System.Drawing.Point(0, 268);
+            this.labelGameHalf.Location = new System.Drawing.Point(0, 335);
             this.labelGameHalf.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.labelGameHalf.Name = "labelGameHalf";
-            this.labelGameHalf.Size = new System.Drawing.Size(337, 65);
+            this.labelGameHalf.Size = new System.Drawing.Size(425, 81);
             this.labelGameHalf.TabIndex = 4;
             this.labelGameHalf.Text = "labelGameHalf";
             // 
@@ -309,23 +318,24 @@ partial class MainWindow
             // 
             this.panel2.Controls.Add(this.pictureBoxMonitor);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel2.Location = new System.Drawing.Point(341, 10);
+            this.panel2.Location = new System.Drawing.Point(426, 12);
+            this.panel2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(915, 662);
+            this.panel2.Size = new System.Drawing.Size(1144, 828);
             this.panel2.TabIndex = 21;
             // 
             // MainWindow
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.BackColor = System.Drawing.SystemColors.Window;
-            this.ClientSize = new System.Drawing.Size(1266, 682);
+            this.ClientSize = new System.Drawing.Size(1582, 852);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "MainWindow";
-            this.Padding = new System.Windows.Forms.Padding(10);
-            this.ShowIcon = false;
+            this.Padding = new System.Windows.Forms.Padding(12, 12, 12, 12);
             this.Text = "EDC Host EDC_HOST_BUILD_REF";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MainWindow_FormClosed);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMonitor)).EndInit();
